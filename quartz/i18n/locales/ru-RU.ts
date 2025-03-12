5f3128749c65fb80c78118d2757a64ff5fc54890
AtomicBim <romangman90@gmail.com>
2025-03-12 11:44:24 +0500

    Обновил создателя

--- quartz/i18n/locales/ru-RU.ts
+++ quartz/i18n/locales/ru-RU.ts
@@ -33,7 +33,7 @@ export default {
       title: "Проводник",
     },
     footer: {
-      createdWith: "Создано с помощью",
+      createdWith: "Создал",
     },
     graph: {
       title: "Вид графа",
